3b439927391753b7093cc4579c572c4328202779
PelayoElygelene <126479095+PelayoElygelene@users.noreply.github.com>
2024-01-12 14:40:30 +0600

    Create ShoppingCart.php

---
+++ ShoppingCart.php
@@ -0,0 +1,24 @@
+// ShoppingCart.php
+class ShoppingCart {
+    private $items;
+
+    public function __construct() {
+        $this->items = [];
+    }
+
+    public function addItem($product) {
+        $this->items[] = $product;
+    }
+
+    public function getItems() {
+        return $this->items;
+    }
+
+    public function calculateTotal() {
+        $total = 0;
+        foreach ($this->items as $item) {
+            $total += $item->getPrice();
+        }
+        return $total;
+    }
+}
